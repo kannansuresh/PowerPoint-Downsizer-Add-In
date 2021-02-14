89793ae3e8c27e125d439f801246e41da402a8cf
Kannan Suresh <kannansuresh@outlook.com>
2021-02-14 16:08:55 +0530

    Updates.

--- src/Aneejian.PowerPoint.Downsizer/Fickles.cs
+++ src/Aneejian.PowerPoint.Downsizer/Fickles.cs
@@ -5,7 +5,7 @@
         public const string ClassLibraryName = "PowerPoint Downsizer";
         public const string AddInName = "PowerPoint Downsizer Add-In";
         public const string WebAddInName = "PowerPoint Downsizer Web Add-In";
-        public const string AddInDescription = "Add-In to downsize PowerPoint presentations by removing unused custom layouts and master slides.";
+        public const string AddInDescription = "Add-In to downsize PowerPoint presentations by removing unused layouts and master slides.";
         public const string NothingToRemove = "Nothing to remove. All layouts and master slides in use.";
 
         public const string HideTabLabel = "Hide the Downsizer tab.";
