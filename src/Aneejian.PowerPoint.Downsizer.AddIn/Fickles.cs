68877bfb95c674b18d13ba578eab797ab36e1baa
Kannan Suresh <kannansuresh@outlook.com>
2021-02-23 09:27:27 +0530

    Added fickle.

---
+++ src/Aneejian.PowerPoint.Downsizer.AddIn/Fickles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aneejian.PowerPoint.Downsizer.AddIn
+{
+    internal static class Fickles
+    {
+        // URLs
+
+        internal const string CompanyUrl = "https://aneejian.com";
+        internal const string RepositoryUrl = "https://kannansuresh.github.io/PowerPoint-Downsizer/";
+        internal const string BlogPostUrl = "https://kannansuresh.github.io/PowerPoint-Downsizer/";
+        internal const string CheckUpdateUrl = "https://kannansuresh.github.io/PowerPoint-Downsizer/update-info.txt";
+
+        // Coffee
+
+        internal const string CoffeeUrl = "https://www.buymeacoffee.com/aneejian";
+        internal const int CoffeeRevealThreshold = 10;
+        internal const bool EnableCoffee = true;
+
+        internal static class AppInfo
+        {
+            // Assembly info
+
+            internal const string Title = "PowerPoint Downsizer Add-In";
+            internal const string Description = "Add-In to downsize PowerPoint presentations by removing unused layouts and master slides.";
+            internal const string Company = "Aneejian";
+            internal const string Product = Title;
+            internal const string Copyright = "Copyright ©  Aneejian 2021";
+            internal const string Version = "v1.0.0";
+        }
+    }
+}
