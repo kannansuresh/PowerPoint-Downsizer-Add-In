7e2495322e735a421f5d78b7f48be0c1d870d9f8
Kannan Suresh <kannansuresh@outlook.com>
2021-03-03 08:36:26 +0530

    Updated.

--- src/Solution Items/GlobalAssemblyInfo.cs
+++ src/Solution Items/GlobalAssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Aneejian 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
 #else
