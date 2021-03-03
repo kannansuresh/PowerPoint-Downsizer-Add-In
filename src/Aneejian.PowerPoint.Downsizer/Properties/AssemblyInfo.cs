b6ac59b8bcd03261a6465f807230e094b3aed0b5
Kannan Suresh <kannansuresh@outlook.com>
2021-03-03 08:34:10 +0530

    Updated assembly version.

--- src/Aneejian.PowerPoint.Downsizer/Properties/AssemblyInfo.cs
+++ src/Aneejian.PowerPoint.Downsizer/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Tool to downsize PowerPoint presentations by removing unused layouts and master slides.")]
 [assembly: ComVisible(true)]
 [assembly: Guid("4813e0d9-b582-4a2c-9d36-7361557eb733")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
